5a9fa61cf95bc4c9a0508b1521954b6ab0d6e93e
Philipp von Seydlitz <pvseydlitz@me.com>
2020-08-29 12:39:26 +0200

    First draft recipies and json fie for data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Recipe from './Recipe'
+import recipes from './recipes.json'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {recipes.map((recipe, index) => (
+        <Recipe recipe={recipe} key={index}></Recipe>
+      ))}
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/Recipe.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import styled from 'styled-components/macro'
+
+export default function Recipe(recipe) {
+  const data = recipe.recipe
+
+  let kosten = ''
+  switch (data.kosten) {
+    case 1:
+      kosten = '€'
+      break
+    case 2:
+      kosten = '€€'
+      break
+    case 3:
+      kosten = '€€€'
+      break
+    case 4:
+      kosten = '€€€€'
+      break
+  }
+
+  return (
+    <Wrapper>
+      <Title>{data.titel}</Title>
+      <Text>{data.beschreibung}</Text>
+      <Headline>Benötigte Zutaten</Headline>
+      <ul>
+        {data.zutaten.map((zutat, index) => (
+          <li key={index}>
+            {zutat.menge} {zutat.einheit} {zutat.produkt}
+          </li>
+        ))}
+      </ul>
+      <Headline>Arbeitsschritte</Headline>
+      <ol>
+        {data.arbeitsschritte.map((arbeitsschritt, index) => (
+          <li key={index}>{arbeitsschritt}</li>
+        ))}
+      </ol>
+      {data.kategorien.map((kategorie, index) => (
+        <Kategorie key={index}>{kategorie}</Kategorie>
+      ))}
+      <Text>Aufwand: {data.aufwand} min</Text>
+      <Text>Kosten: {kosten}</Text>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+  background-color: lightgrey;
+  border-radius: 15px;
+  margin: 15px;
+  padding: 10px;
+`
+const Title = styled.p`
+  margin: 0;
+  font-weight: bold;
+`
+const Text = styled.div`
+  font-size: 14px;
+`
+const Headline = styled.h3``
+
+const Kategorie = styled.div`
+  background-color: grey;
+  padding: 5px 10px;
+  display: inline-block;
+  margin: 10px;
+  border-radius: 5px;
+`
